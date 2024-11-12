497691c95048a66266e61c9997041059672ee5f9
DCorradini <corradinidavid@gmail.com>
2024-11-12 09:25:19 +0700

    Adaptation 6.7
    Move off canvas script to own file
    Change theme.json version
    Add style folder (need cleaning)

--- scripts/dc24-front.js
+++ scripts/dc24-front.js
@@ -1,18 +1,5 @@
-
-jQuery(document).ready(function ($) {
-    function updateSearchIcon() {
-        $('.facetwp-facet-search ').find("i").removeClass('facetwp-icon').addClass('fas fa-search test');
-    }
-
-    // Initial update when document is ready
-    updateSearchIcon();
-
-    // Update after FacetWP is fully loaded
-    $(document).on('facetwp-loaded', function () {
-        updateSearchIcon();
-    });
-
-});
+// import "./dc24-offcanvas.js";
+// import "./dc24-animation.js";
 
 
 
