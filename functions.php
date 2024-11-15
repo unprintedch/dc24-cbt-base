34a3878aec235551f662f0e2a29691da5189652a
DCorradini <corradinidavid@gmail.com>
2024-11-15 14:28:36 +0700

    Cleanup_2

--- functions.php
+++ functions.php
@@ -1,16 +1,28 @@
 <?php
 require_once get_template_directory() . '/functions/dc24-functions.php';
-require_once get_template_directory() . '/functions/dc24-acf.php';
-require_once get_template_directory() . '/functions/dc24-block-register.php';
-require_once get_template_directory() . '/functions/dc24-enqueue.php';
-require_once get_template_directory() . '/functions/dc24-block-styles.php';
-require_once get_template_directory() . '/functions/dc24-menu-walker.php';
-require_once get_template_directory() . '/functions/dc24-facet.php';
-require_once get_template_directory() . '/functions/dc24-gravityform.php';
-require_once get_template_directory() . '/functions/dc24-acf.php';
-require_once get_template_directory() . '/functions/dc24-safelist-update.php';
-// require_once get_template_directory() . '/functions/dc24-wpml.php';
 
+
+require_once get_template_directory() . '/functions/dc24-enqueue.php';
+// register acf block (automatique si le fichier est dans le dossier blocks)
+require_once get_template_directory() . '/functions/dc24-block-register.php';
+//regsieter block styles
+require_once get_template_directory() . '/functions/dc24-block-styles.php';
+// Quand on sauve un post, cela check les classes tailwind et les ajoute au safelist
+require_once get_template_directory() . '/functions/dc24-safelist-update.php';
+// Observe les couleurs et les styles des heading pour les copier dans tailwind
+require_once get_template_directory() . '/functions/dc24-theme-styles.php';
+
+
+//Plugin specific functions
+// require_once get_template_directory() . '/functions/dc24-acf.php';
+// require_once get_template_directory() . '/functions/dc24-facet.php';
+// require_once get_template_directory() . '/functions/dc24-gravityform.php';
+// require_once get_template_directory() . '/functions/dc24-acf.php';
+
+// Au cas ou on a besoin de WPML
+// require_once get_template_directory() . '/functions/dc24-wpml.php';
+// Si on utilise le menu "hybride"
+// require_once get_template_directory() . '/functions/dc24-menu-walker.php';
 
 
 
--- functions/dc24-enqueue.php
+++ functions/dc24-enqueue.php
@@ -3,18 +3,6 @@ add_action('wp_enqueue_scripts', 'dc24_enqueue_styles');
 function dc24_enqueue_styles()
 {
   
-  wp_enqueue_style(
-    'monotype-fonts',
-    'https://cdn.fonts.net/kit/d9e95a14-3f4f-4569-ac9a-fda1668b7e2f/d9e95a14-3f4f-4569-ac9a-fda1668b7e2f.css',
-    array(),
-    null
-  );
-  
-  // Add preconnect link for optimization
-  add_action('wp_head', function() {
-    echo '<link rel="preconnect" href="https://cdn.fonts.net">';
-  });
-  
   wp_enqueue_style('front-styles', get_template_directory_uri() . '/build/style.css');
   wp_enqueue_script('front-scripts', get_template_directory_uri() . '/build/app.js', array('jquery'), null, true);
   wp_localize_script('front-scripts', 'ajax_object', array(
@@ -35,32 +23,17 @@ function dc24_enqueue_block_assets() {
 add_action('enqueue_block_assets', 'dc24_enqueue_block_assets');
 
 
-function mytheme_enqueue_block_editor_fonts() {
-  wp_enqueue_style(
-      'monotype-fonts-editor',
-      'https://cdn.fonts.net/kit/d9e95a14-3f4f-4569-ac9a-fda1668b7e2f/d9e95a14-3f4f-4569-ac9a-fda1668b7e2f.css',
-      array(),
-      null
-  );
-  add_action('enqueue_block_editor_assets', function() {
-      echo '<link rel="preconnect" href="https://cdn.fonts.net">';
-      echo '<link href="https://cdn.fonts.net/kit/d9e95a14-3f4f-4569-ac9a-fda1668b7e2f/d9e95a14-3f4f-4569-ac9a-fda1668b7e2f.css" rel="stylesheet">';
-  });
-}
-add_action('enqueue_block_assets', 'mytheme_enqueue_block_editor_fonts');
+
 
 // function enqueue_slider_block_assets()
 // {
 //   global $post;
-
 //   if (has_block('dc24/slider', $post) || has_block('dc24/slider-items', $post) || has_block('dc24/slider-video', $post)) {
 //     // Enqueue Swiper CSS
 //     // wp_enqueue_style('swiper-css', 'https://unpkg.com/swiper/swiper-bundle.min.css');
-//     // wp_enqueue_style('swiper-css', get_theme_file_uri("/package-local/package/swiper-bundle.min.css"));
 
 //     // Enqueue Swiper JS
 //     // wp_enqueue_script('swiper-js', 'https://unpkg.com/swiper/swiper-bundle.min.js', array(), null, true);
-//     // wp_enqueue_script('swiper-js', get_theme_file_uri("/package-local/package/swiper-bundle.min.js"), array(), null, true);
 //   }
 // }
 // add_action('wp_enqueue_scripts', 'enqueue_slider_block_assets');
